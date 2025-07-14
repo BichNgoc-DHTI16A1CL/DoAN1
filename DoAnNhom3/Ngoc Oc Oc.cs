38ebd2bed54dd301a882a69c86c822d375cca793
BichNgoc-DHTI16A1CL <139781670+BichNgoc-DHTI16A1CL@users.noreply.github.com>
2025-07-14 17:33:27 +0700

    Create Ngoc Oc Oc.cs
    Ngọc Ọc ọc

---
+++ DoAnNhom3/Ngoc Oc Oc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnNhom3
+{
+    class Ngoc_Oc_Oc
+    {
+    }
+}
